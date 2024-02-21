0b0cff7d6982603d0da5eed0419996c9891ddcb2
Teon L Brooks <teon.brooks@gmail.com>
2024-02-20 23:09:13 -0500

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <!-- This is the global layout file; it "wraps" every page on the site. (Or more accurately: is the parent component to every page component on the site.) -->
 <script>
 	import { siteTitle, siteURL } from '$lib/config.js';
-	import '$lib/assets/scss/global.scss'
 	// use default svelte-material-ui css
 	import '$lib/../../node_modules/svelte-material-ui/bare.css'
 	import Header from '$lib/components/Header.svelte'
