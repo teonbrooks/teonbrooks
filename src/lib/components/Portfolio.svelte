bf58363b83395b8f0c81bf80c5cfc2c626aca105
Teon L Brooks <teon.brooks@gmail.com>
2024-03-23 15:42:17 -0400

    add button filter to my portfolio page :))
    lots of clean up and some formatting

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -1,57 +1,53 @@
 <script>
-    import LayoutGrid, { Cell } from '@smui/layout-grid';
-    import Snackbar, {Actions as SBActions, Label as SBLabel} from '@smui/snackbar';
-    import { mdiLink, mdiCloseCircle } from '@mdi/js';
-
-    import Card, {
-        Content,
-        PrimaryAction,
-        Actions,
-        ActionButtons,
-        ActionIcons,
-    } from '@smui/card';
-    import Button, { Label } from '@smui/button';
-    import IconButton, { Icon } from '@smui/icon-button';
+	import LayoutGrid, { Cell } from '@smui/layout-grid';
 	import PortfolioCard from './PortfolioCard.svelte';
 
-    let snackbarWithClose;
-    export let items = [];
-    // export let categories = items.map(x => x.category).filter((v, i, a) => a.indexOf(v) === i);
-    export let path_icons = '/images/portfolio_icons';
+	export let items = [];
+	export let categories = items
+		.map((x) => x.category)
+		.flat(Infinity)
+		.filter((v, i, a) => a.indexOf(v) === i);
 
+	export let path_icons = '/images/portfolio_icons';
+	$: isClicked = '';
+	$: matches = (item) => {
+		if (!Array.isArray(item.category)) {
+			item.category = [item.category];
+		}
+		if (!isClicked) {
+			return item;
+		}
+		return item.category.includes(isClicked) ? item : false;
+	};
 </script>
 
 <article id="portfolio" class="panel">
-    <div style="display: flex; align-items: center;">
-        <section id="page-portfolio" class="page-portfolio">
-            <h3 class="section-subtitle">Collection of my work</h3>
-            <div id="filters" class="button-group">
-            <!-- console.log(Object.groupBy(items, ({ category }) => category)) -->
-                <!-- <LayoutGrid>
-                <Cell><button>Show All</button></Cell>
-                {#each categories as category}
-                    <Cell><button class="button" data-filter=".{category}">{category}</button></Cell>
-                {/each}
-            </LayoutGrid> -->
-            </div>
-        <LayoutGrid>
-            {#each items as item}
-                <Cell>
-                    <PortfolioCard {item} {path_icons}/>
-                </Cell>
-            {/each}
-        </LayoutGrid>
-    </section>
-
+	<div style="display: flex; align-items: center;">
+		<section id="page-portfolio" class="page-portfolio">
+			<h3 class="section-subtitle">Collection of my work</h3>
+			<div id="filters" class="button-group">
+				<LayoutGrid>
+					<Cell span={4}><button on:click={() => (isClicked = '')}>Show All</button></Cell>
+					{#each categories as category}
+						<Cell
+							><button
+								class="button"
+								data-filter=".{category}"
+								on:click={() => {
+									isClicked = category;
+								}}>{category}</button
+							></Cell
+						>
+					{/each}
+				</LayoutGrid>
+			</div>
+			<LayoutGrid>
+				{#each items.filter(matches) as item}
+					<Cell>
+						<PortfolioCard {item} {path_icons} />
+					</Cell>
+				{/each}
+			</LayoutGrid>
+		</section>
+	</div>
 </article>
-
-<style>
-    .demo-cell {
-        height: 60px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: var(--mdc-theme-secondary, #333);
-        color: var(--mdc-theme-on-secondary, #fff);
-  }
-</style>
--- src/lib/components/PortfolioCard.svelte
+++ src/lib/components/PortfolioCard.svelte
@@ -1,69 +1,71 @@
 <script>
-    export let path_icons;
-    export let item;
-    import { mdiLink, mdiClose } from '@mdi/js';
-    import Card, {
-            Content,
-            Actions,
-            ActionButtons,
-            ActionIcons,
-        } from '@smui/card';
-    import Button, { Label } from '@smui/button';
-    import IconButton, { Icon } from '@smui/icon-button';
-  import Dialog, { Content as DContent } from '@smui/dialog';
-  import markdownit from 'markdown-it';
+	export let path_icons;
+	export let item;
+	import { mdiLink, mdiClose } from '@mdi/js';
+	import Card, { Content, Actions, ActionButtons, ActionIcons } from '@smui/card';
+	import Button, { Label } from '@smui/button';
+	import IconButton, { Icon } from '@smui/icon-button';
+	import Dialog, { Content as DContent } from '@smui/dialog';
+	import markdownit from 'markdown-it';
 
-  const md = markdownit();
-  let open = false;
-
+	const md = markdownit();
+	let open = false;
 </script>
 
-
 <Card>
-    <Content>
-        {#if item.icon}
-            <img src="{path_icons}/{item.icon}" alt={item.organization} class={item.categories}>    
-        {/if}
-    </Content>
-    <Actions style=align-items:end>
-        <ActionButtons>
-            <Button on:click={() => (open = true)}>
-                <Label>Details</Label>
-            </Button>
-        </ActionButtons>
-        <ActionIcons>    
-            <IconButton on:click={() => window.open(item.website)} title="Open Link">
-                <Icon tag="svg" viewBox="0 0 24 24">
-                    <path fill="currentColor" d={mdiLink} />
-                </Icon>
-            </IconButton>
-        </ActionIcons>
-    </Actions>
+	<Content>
+		{#if item.icon}
+			<img src="{path_icons}/{item.icon}" alt={item.organization} class={item.categories} />
+		{/if}
+	</Content>
+	<Actions style="align-items:end">
+		<ActionButtons>
+			<Button on:click={() => (open = true)}>
+				<Label>Details</Label>
+			</Button>
+		</ActionButtons>
+		<ActionIcons>
+			<IconButton on:click={() => window.open(item.website)} title="Open Link">
+				<Icon tag="svg" viewBox="0 0 24 24">
+					<path fill="currentColor" d={mdiLink} />
+				</Icon>
+			</IconButton>
+		</ActionIcons>
+	</Actions>
 </Card>
 <Dialog bind:open sheet aria-describedby="sheet-content">
-    <DContent id="sheet-content">
-        <IconButton action="close" class="material-icons">
-            <Icon tag="svg" viewBox="0 0 24 24">
-                <path fill="currentColor" d={mdiClose} />
-            </Icon>
-        </IconButton>
-        <div>
-            {#if item.icon}
-                <img src="{path_icons}/{item.icon}" alt={item.organization} class={item.categories} width="100" height="100">    
-            {/if}
-            <h3>{item.title}</h3>
-            <p><em>{item.timespan}</em></p>
-            <p>
-                {@html md.render(item.description)}
-            </p>
-        </div>
-    </DContent>
+	<DContent id="sheet-content">
+		<IconButton action="close" class="material-icons">
+			<Icon tag="svg" viewBox="0 0 24 24">
+				<path fill="currentColor" d={mdiClose} />
+			</Icon>
+		</IconButton>
+		<div>
+			{#if item.icon}
+				<img
+					src="{path_icons}/{item.icon}"
+					alt={item.organization}
+					class={item.categories}
+					width="100"
+					height="100"
+				/>
+			{/if}
+			<h1>{item.title}</h1>
+			<p><em>{item.timespan}</em></p>
+			<p>
+				{@html md.render(item.description)}
+			</p>
+		</div>
+	</DContent>
 </Dialog>
 
 <style>
-    img {
-        width: 150px;
-        height: 150px;
-    }
+	img {
+		width: 150px;
+		height: 150px;
+	}
 
+	h1 {
+		font-size: x-large;
+	}
 </style>
