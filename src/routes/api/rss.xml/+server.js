1f9d8a98367948caf2a629bfeadc81c01a575855
Teon L Brooks <teon.brooks@gmail.com>
2026-03-14 18:41:48 -0400

    Update +server.js
    Render the full html in the feed

--- src/routes/api/rss.xml/+server.js
+++ src/routes/api/rss.xml/+server.js
@@ -1,59 +1,52 @@
-// IMPORTANT: update all these property values in src/lib/config.js
 import { siteTitle, siteDescription, siteURL } from '$lib/config'
+import { render } from 'svelte/server'
+import fetchPosts from '$lib/assets/js/fetchPosts'
 
 export const prerender = true
 
-export const GET = async () => {	
-	const data = await Promise.all(
-		Object.entries(import.meta.glob('$lib/posts/*.md')).map(async ([path, page]) => {
-			const blogPost = await page()
-			// TODO: add blog content to the rss feed. Consider atom format
-			// const blogContent = blogPost.default.render().html
-			const metadata = blogPost.metadata
-			const slug = path.split('/').pop().split('.').shift()
-			return { ...metadata, slug }
+export const GET = async () => {
+	const { posts } = await fetchPosts({ limit: -1 })
+
+	// Render each post's body HTML via its mdsvex component
+	const postModules = import.meta.glob('/src/lib/posts/*.md')
+	const htmlBySlug = {}
+	await Promise.all(
+		Object.entries(postModules).map(async ([path, resolver]) => {
+			const { default: Component } = await resolver()
+			const { html } = render(Component)
+			const slug = path.split('/').pop().slice(0, -3)
+			htmlBySlug[slug] = html
 		})
 	)
-	.then(posts => {
-		return posts.sort((a, b) => new Date(b.date) - new Date(a.date))
-	})
 
-	const body = render(data)
+	const postsWithHtml = posts.map(post => ({ ...post, html: htmlBySlug[post.slug] ?? '' }))
+
+	const body = renderFeed(postsWithHtml)
 	const options = {
 		headers: {
 			'Cache-Control': `max-age=0, s-max-age=${600}`,
 			'Content-Type': 'application/xml',
 		}
 	}
-	return new Response(
-		body,
-		options,
-	)
-};
+	return new Response(body, options)
+}
 
-
-//Be sure to review and replace any applicable content below!
-const render = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+const renderFeed = (posts) => `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/" xmlns:content="http://purl.org/rss/1.0/modules/content/">
 <channel>
 <title>${siteTitle}</title>
 <description>${siteDescription}</description>
 <link>${siteURL}</link>
 <atom:link href="${siteURL}/api/rss.xml" rel="self" type="application/rss+xml"/>
-${posts
-	.map(
-		(post) => `<item>
+${posts.map((post) => `<item>
 <guid isPermaLink="true">${siteURL}/blog/${post.slug}</guid>
 <title>${post.title}</title>
-<image>
-	<url>${siteURL}${post.coverImage}</url>
-</image>
 <link>${siteURL}/blog/${post.slug}</link>
-<description>${post.excerpt}</description>
-<pubDate>${new Date(post.date).toUTCString()}</pubDate>
-</item>`
-	)
-	.join('')}
+<description>${post.excerpt ?? ''}</description>
+<pubDate>${new Date(post.date).toUTCString()}</pubDate>${post.coverImage ? `
+<media:content url="${siteURL}${post.coverImage}" medium="image"/>` : ''}
+<content:encoded><![CDATA[${post.html}]]></content:encoded>
+</item>`).join('')}
 </channel>
 </rss>
-`;
+`
